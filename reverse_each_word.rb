6e0c0de9a307dc1097a874138502c78749fa66a2
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 23:47:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,5 +12,4 @@ def reverse_each_word(sentence)
   new_array = sentence.split
   reversed_array = new_array.map {|element| element.reverse}
   reversed_array.join(" ")
-  reversed_array.collect
 end
